7f21427178810a634fd208e087349387feeae9ad
root <root@LNG-BPQ0HK3.localdomain>
2024-12-08 14:45:51 -0500

    complete persona creation code

--- data/create_personas.py
+++ data/create_personas.py
@@ -1,17 +1,95 @@
 """
 """
 import os
+import asyncio
 import argparse
 import jsonlines
-
+from pydantic import BaseModel
+from concurrent.futures import ThreadPoolExecutor
 from datasets import load_dataset
+from tqdm import tqdm
 
-def main(args):
-    pass
+from persona_generation.prompt_template import PERSONA_GENERATION
+from litellm_client import get_completion, Generation_Models
+
+
+DATASET_NAME="wikimedia/wikipedia"
+
+class Persona(BaseModel):
+  countries: list[str]
+  languages: list[str]
+  persona: str
+
+class PersonaList(BaseModel):
+    persona_list: list[Persona]
+
+def prompt_processor(example):
+
+    # get the first 200 words of the text
+    text = example["text"].split()[:200]
+    text = " ".join(text)
+
+    prompt = [ {"role": "system", "content": PERSONA_GENERATION, "cache_control": {"type": "ephemeral"}}, 
+              {"role": "user", "content": text}]
+    
+    example['templated_prompt'] = prompt
+    return example
+
+
+def batch_dataset_generator(dataset, batch_size):
+
+    # Create prompt
+    dataset = dataset.map(prompt_processor)
+
+    for i in range(0, len(dataset), batch_size):
+        yield dataset[i:i+batch_size]
+
+
+async def main(args):
+    dataset = load_dataset(DATASET_NAME, f"20231101.{args.language}")
+    model = Generation_Models
+
+    # create a text file for managing processed pages
+    file_path = f"{args.data_directory}/{args.language}_processed_urls.txt"
+
+    if os.path.exists(file_path):
+        with open(file_path, "r") as f:
+            processed_pages = f.read().splitlines()
+    else:
+        processed_pages = []
+    
+    # filter dataset to pages that have more than 1000 words
+    dataset = dataset.filter(lambda x: len(x["text"].split()) > 500)
+
+    print(f"Processing {len(dataset['train'])} pages")
+
+    # Remove pages that have already been processed
+    dataset = dataset.filter(lambda x: str(x["id"]) not in set(processed_pages))
+
+    print(f"Processing {len(dataset['train'])} pages")
+    
+    with jsonlines.open(f"{args.data_directory}/{args.language}_personas.jsonl", "a") as writer, open(file_path, "a") as f:
+        # iterate over the dataset and use multiprocessing to generate personas
+
+        for i, batch in tqdm(enumerate(batch_dataset_generator(dataset['train'], args.batch_size)), total=len(dataset)//args.batch_size):
+            print(f"Processing batch {i}")
+
+            results = await get_completion(batch['templated_prompt'], Generation_Models.GPT4O_MINI, PersonaList)
+
+            for i, id, url, res in zip(range(len(batch)), batch["id"], batch["url"], results):
+                model_used = res.model
+                for persona in res.generation['persona_list']:
+                    writer.write({"id": id, "url": url, "persona": persona, "model": model_used})
+                f.write(f"{id}\n")
+
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--data_directory", type=str, default="files")
+    parser.add_argument("--language", type=str, default="yo")
+    parser.add_argument("--batch_size", type=int, default=10)
+    parser.add_argument("--model", type=Generation_Models, choices=list(Generation_Models), default=Generation_Models.GPT4O_MINI)
     args = parser.parse_args()
-    main(args)
 
+    asyncio.run(main(args))
--- data/litellm_client.py
+++ data/litellm_client.py
@@ -1,5 +1,5 @@
 import json
-
+import openai
 from enum import Enum
 from typing import List, Dict, Any
 from dataclasses import dataclass
@@ -7,7 +7,7 @@ from dataclasses import dataclass
 from tenacity import (
     retry,
     stop_after_attempt,
-    wait_random_exponential,
+    wait_fixed,
 )
 from litellm import acompletion, batch_completion
 
@@ -17,12 +17,15 @@ class Generation_Models(Enum):
     CLAUDE_SONNET = "claude-3-5-sonnet-20240620"
     CLAUDE_HAIKU = "claude-3-haiku-20240307"
 
+    def __str__(self):
+        return self.value
+
 @dataclass
 class ModelCompletion:
     generation: Dict[str, Any]
     model: str
 
-@retry(wait=wait_random_exponential(min=1, max=60), stop=stop_after_attempt(3))
+@retry(wait=wait_fixed(120), stop=stop_after_attempt(10))
 async def get_completion(prompt: List[Dict[str, str]] | List[List[Dict[str, str]]],
             model: Generation_Models,
             structured_object: Any,
@@ -33,37 +36,51 @@ async def get_completion(prompt: List[Dict[str, str]] | List[List[Dict[str, str]
     """
     completions = []
 
-    if isinstance(prompt[0], list):
-        response = batch_completion(
-            model=model.value,
-            messages=prompt,
-            temperature=temperature,
-            max_tokens=max_tokens,
-            response_format=structured_object
-        )
+    try:
+        if isinstance(prompt[0], list):
+            response = batch_completion(
+                model=model.value,
+                messages=prompt,
+                temperature=temperature,
+                max_tokens=max_tokens,
+                response_format=structured_object
+            )
+            
+            for model_resp in response:
+                try:
+                    completions.append(ModelCompletion(generation=json.loads(model_resp.choices[0].message.content), model=model.value))
+                except json.JSONDecodeError:
+                    completions.append(ModelCompletion(generation=model_resp.choices[0].message.content, model=model.value))
 
-        for model_resp in response:
+        elif isinstance(prompt[0], dict):
+            response = await acompletion(
+                model=model.value,
+                messages=prompt,
+                temperature=temperature,
+                max_tokens=max_tokens,
+                response_format=structured_object
+            )
+
             try:
-                completions.append(ModelCompletion(generation=json.loads(model_resp.choices[0].message.content), model=model.value))
+                completions.append(ModelCompletion(generation=json.loads(response.choices[0].message.content), model=model.value))
             except json.JSONDecodeError:
-                completions.append(ModelCompletion(generation=model_resp.choices[0].message.content, model=model.value))
+                completions.append(ModelCompletion(generation=response.choices[0].message.content, model=model.value))
 
-    elif isinstance(prompt[0], dict):
-        response = await acompletion(
-            model=model.value,
-            messages=prompt,
-            temperature=temperature,
-            max_tokens=max_tokens,
-            response_format=structured_object
-        )
-
-        try:
-            completions.append(ModelCompletion(generation=json.loads(response.choices[0].message.content), model=model.value))
-        except json.JSONDecodeError:
-            completions.append(ModelCompletion(generation=response.choices[0].message.content, model=model.value))
-
-    else:
-        raise ValueError("Prompt should be a list of dictionaries or a list of lists of dictionaries")
+        else:
+            raise ValueError("Prompt should be a list of dictionaries or a list of lists of dictionaries")
+    
+    except openai.APITimeoutError as e:
+        print(f"API Timeout Error: {e}")
+        raise e
+    except openai.APIConnectionError as e:
+        print(f"API Connection Error: {e}")
+        raise e
+    except openai.RateLimitError as e:
+        print(f"Rate Limit Error: {e}")
+        raise e
+    except Exception as e:
+        print(f"Error: {e}")
+        raise e
 
     return completions
 
