e8323fa368f19072d4e6faf3f112d7844cc9cdbe
root <ogundepoodunayo@gmail.com>
2024-12-11 22:21:06 -0500

    more training code

--- src/training/sft.py
+++ src/training/sft.py
@@ -1,24 +1,83 @@
+"""
+Some Training Notes:
+
+local_main_process typically refers to the main process on a specific node/machine in a distributed setup. There can be multiple local_main_processes - one per node.
+main_process (or global main process) refers to the single primary process that coordinates across all nodes in the distributed system. There is only one main_process across the entire training setup.
+"""
+
+import os
 import time
+import logging
+import datasets
+import transformers
 
 from datetime import timedelta
 from dataclasses import dataclass, field
 from typing import Optional, List, Union
 from accelerate.utils import InitProcessGroupKwargs, set_seed, DataLoaderConfiguration
 from accelerate import Accelerator
+from accelerate.logging import get_logger
+from datasets import load_dataset
 
-from utils import ArgumentParserPlus
+from transformers import AutoConfig
+
+from utils import ArgumentParserPlus, mix_datasets
+
+logger = get_logger(__name__)
 
 @dataclass
-class SFTArguments:
+class DatasetArguments:
     """
-    Arguments for SFT training.
+    Arguments for dataset configuration.
     """
-    model_name_or_path:str = field(
+    dataset_name: Optional[str] = field(
+        default=None,
+        metadata={"help": "The name of the huggingface dataset to use, the expectation is that this is an existing dataset mixture"}
+    )
+    dataset_config_name: Optional[str] = field(
+        default=None,
+        metadata={"help": "The configuration name of the dataset to use"}
+    )
+    dataset_mixer: Optional[dict] = field(
+        default=None, metadata={"help": "A dictionary of datasets (local or HF) to sample from."}
+    )
+    dataset_mixer_list: Optional[list[str]] = field(
+        default=None, metadata={"help": "A list of datasets (local or HF) to sample from."}
+    )
+    dataset_mix_dir: Optional[str] = field(
+        default=None, metadata={"help": "The directory to save the mixed dataset to disk."}
+    )
+    train_file: Optional[str] = field(
+        default=None,
+        metadata={"help": "The path to the training file"}
+    )
+
+@dataclass
+class ModelArguments:
+    """
+    Arguments for model configuration.
+    """
+    model_name_or_path: str = field(
         metadata={"help": "The model checkpoint for weights initialization."}
     )
-    model_revision:str = field(
+    model_revision: str = field(
+        default="main",
         metadata={"help": "The version of the model on huggingface to use"}
     )
+    config_name: Optional[str] = field(
+        default=None,
+        metadata={"help": "The model configuration to use, it is usually a model name"}
+    )
+    trust_remote_code: Optional[bool] = field(
+        default=True,
+        metadata={"help": "Whether to trust remote code"}
+    )
+
+@dataclass
+class ExperimentArguments:
+    """
+    Arguments for experiment configuration.
+    """
     exp_name: str = field(
         metadata={"help": "The name of the experiment"}
     )
@@ -29,6 +88,10 @@ class SFTArguments:
     push_to_hub: bool = field(
         default=False,
         metadata={"help": "Whether to push the model to the hub"}
+    )
+    output_dir: Optional[str] = field(
+        default=None,
+        metadata={"help": "The output directory to save the model and logs"}
     )
     with_tracking: bool = field(
         default=False,
@@ -46,10 +109,6 @@ class SFTArguments:
             "Options are 'tensorboard', 'wandb', 'comet_ml', 'clearml', or 'all'. "
             "Specify multiple by listing them: e.g., ['tensorboard', 'wandb']"
         },
-    )
-    dataset_name: Optional[str] = field(
-        default=None,
-        metadata={"help": "The name of the huggingface dataset to use, the expectation is that this is an existing dataset mixture"}
     )
     hf_repo_id: Optional[str] = field(
         default=None,
@@ -72,45 +131,128 @@ class SFTArguments:
         metadata={"help": "The timeout for the run"}
     )
 
+
+# @dataclass
+# class TrainingArguments(ExperimentArguments, ModelArguments, DatasetArguments):
+#     """
+#     Arguments for training configuration.
+#     """
+#     pass
+
     
 
 def main(args: ArgumentParserPlus):
-    args.run_name = f"{args.exp_name}__{args.seed}__{int(time.time())}"
-    if args.push_to_hub:
-        args.run_name = f"{args.run_name}__hub"
 
-        if args.hf_entity is None:
+    exp_args, model_args, data_args = args[0], args[1], args[2]
+
+    print(exp_args)
+
+    exp_args.run_name = f"{exp_args.exp_name}__{exp_args.seed}__{int(time.time())}"
+    if exp_args.push_to_hub:
+        exp_args.run_name = f"{exp_args.run_name}__hub"
+
+        if exp_args.hf_entity is None:
             args.hf_entity = "taresco"
-        if args.hf_repo_id is None:
-            args.hf_repo_id = f"{args.hf_entity}/{args.exp_name}"
-        if args.hf_repo_revision is None:
-            args.hf_repo_revision = args.run_name
+        if exp_args.hf_repo_id is None:
+            exp_args.hf_repo_id = f"{exp_args.hf_entity}/{exp_args.exp_name}"
+        if exp_args.hf_repo_revision is None:
+            exp_args.hf_repo_revision = exp_args.run_name
 
-        args.hf_repo_url = f"https://huggingface.co/{args.hf_repo_id}/tree/{args.hf_repo_revision}"
+        exp_args.hf_repo_url = f"https://huggingface.co/{exp_args.hf_repo_id}/tree/{exp_args.hf_repo_revision}"
 
     accelerator_log_kwargs = {}
 
-    if args.with_tracking:
-        accelerator_log_kwargs["log_with"] = args.report_to
-        accelerator_log_kwargs["project_dir"] = args.output_dir
+    if exp_args.with_tracking:
+        accelerator_log_kwargs["log_with"] = exp_args.report_to
+        accelerator_log_kwargs["project_dir"] = exp_args.output_dir
 
      # if you get timeouts (e.g. due to long tokenization) increase this.
-    timeout_kwargs = InitProcessGroupKwargs(timeout=timedelta(seconds=args.timeout))
+    timeout_kwargs = InitProcessGroupKwargs(timeout=timedelta(seconds=exp_args.timeout))
     dataloader_config = DataLoaderConfiguration()
     dataloader_config.use_seedable_sampler = True
 
     accelerator = Accelerator(
-        gradient_accumulation_steps=args.gradient_accumulation_steps,
+        gradient_accumulation_steps=exp_args.gradient_accumulation_steps,
         dataloader_config=dataloader_config,
         **accelerator_log_kwargs,
         kwargs_handlers=[timeout_kwargs],
     )
 
+     # Make one log on every process with the configuration for debugging.
+    logging.basicConfig(
+        format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
+        datefmt="%m/%d/%Y %H:%M:%S",
+        level=logging.INFO,
+    )
+    logger.info(accelerator.state, main_process_only=False)
+
+    if accelerator.is_local_main_process:
+        datasets.utils.logging.set_verbosity_warning()
+        transformers.utils.logging.set_verbosity_info()
+    else:
+        datasets.utils.logging.set_verbosity_error()
+        transformers.utils.logging.set_verbosity_error()
     
-    print(args)
+    if exp_args.seed:
+        set_seed(exp_args.seed)
+
+    # Create output directory in the main process
+    if accelerator.is_main_process:
+        if exp_args.output_dir is not None:
+            os.makedirs(exp_args.output_dir, exist_ok=True)
+
+    accelerator.wait_for_everyone()
+
+    if data_args.dataset_name:
+        dataset = datasets.load_dataset(
+            data_args.dataset_name,
+            data_args.dataset_config_name,
+        )
+    elif data_args.dataset_mixer:
+        dataset = mix_datasets(
+            data_args.dataset_mixer,
+            configs=data_args.dataset_config_name,
+            splits=["train"],
+            save_data_dir=data_args.dataset_mix_dir if accelerator.is_main_process else None,
+            columns_to_keep=["messages"],
+        )
+    elif args.dataset_mixer_list:
+        dataset = mix_datasets(
+            data_args.dataset_mixer_list,
+            configs=args.dataset_config_name,
+            splits=["train"],
+            save_data_dir=data_args.dataset_mix_dir if accelerator.is_main_process else None,
+            columns_to_keep=["messages"],
+        )
+    else:
+        data_files = {}
+        dataset_args = {}
+        if data_args.train_file is not None:
+            data_files["train"] = data_args.train_file
+        raw_datasets = load_dataset(
+            "json",
+            data_files=data_files,
+            **dataset_args,
+        )
+
+    
+    # load pretrained model and tokenizer
+    if model_args.config_name:
+        config = AutoConfig.from_pretrained(
+            model_args.config_name,
+            revision=model_args.model_revision,
+            trust_remote_code=model_args.trust_remote_code,
+        )
+    elif model_args.model_name_or_path:
+        config = AutoConfig.from_pretrained(
+            model_args.model_name_or_path,
+            revision=model_args.model_revision,
+            trust_remote_code=model_args.trust_remote_code,
+        )
+        
 
 
 if __name__ == "__main__":
-    parser = ArgumentParserPlus(SFTArguments)
+    parser = ArgumentParserPlus((ExperimentArguments, ModelArguments, DatasetArguments))
     args = parser.parse()
     main(args)
--- src/training/utils.py
+++ src/training/utils.py
@@ -2,9 +2,11 @@ import os
 import sys
 import dataclasses
 
+from datasets import DatasetDict, concatenate_datasets, load_dataset, load_from_disk
 from dataclasses import dataclass, fields
 from typing import List, Optional, Union, Tuple, Any, NewType
 from transformers import HfArgumentParser
+from datasets.builder import DatasetGenerationError
 
 DataClassType = NewType("DataClassType", Any)
 
@@ -82,3 +84,327 @@ class ArgumentParserPlus(HfArgumentParser):
         if len(output) == 1:
             output = output[0]
         return output
+
+# functions for handling different formats of messages
+def convert_alpaca_gpt4_to_messages(example):
+    """
+    Convert an instruction in inst-output to a list of messages.
+    e.g. vicgalle/alpaca-gpt4"""
+    messages = [
+        {
+            "role": "user",
+            "content": (
+                "Below is an instruction that describes a task, paired with an input that provides "
+                "further context. Write a response that appropriately completes the request.\n\n"
+                f"### Instruction:\n{example['instruction']}\n\n"
+                f"### Input:\n{example['input']}\n\n"
+                "### Response:"
+            ),
+        },
+        {"role": "assistant", "content": example["output"]},
+    ]
+    example["messages"] = messages
+    return example
+
+def convert_codefeedback_single_turn_to_messages(example):
+    """
+    Convert a query-answer pair to a list of messages.
+    e.g. m-a-p/CodeFeedback-Filtered-Instruction"""
+    messages = [
+        {"role": "user", "content": example["query"]},
+        {"role": "assistant", "content": example["answer"]},
+    ]
+    example["messages"] = messages
+    return example
+
+def convert_metamath_qa_to_messages(example):
+    """
+    Convert a query-response pair to a list of messages.
+    e.g. meta-math/MetaMathQA"""
+    messages = [
+        {"role": "user", "content": example["query"]},
+        {"role": "assistant", "content": example["response"]},
+    ]
+    example["messages"] = messages
+    return example
+
+def convert_code_alpaca_to_messages(example):
+    """
+    Convert a prompt-completion pair to a list of messages.
+    e.g. HuggingFaceH4/CodeAlpaca_20K"""
+    messages = [
+        {"role": "user", "content": example["prompt"]},
+        {"role": "assistant", "content": example["completion"]},
+    ]
+    example["messages"] = messages
+    return example
+
+def convert_open_orca_to_messages(example):
+    """
+    Convert a question-response pair to a list of messages.
+    e.g. Open-Orca/OpenOrca"""
+    messages = [
+        {"role": "system", "content": example["system_prompt"]},
+        {"role": "user", "content": example["question"]},
+        {"role": "assistant", "content": example["response"]},
+    ]
+    example["messages"] = messages
+    return example
+
+
+def conversations_to_messages(example):
+    """
+    Convert from conversations format to messages.
+
+    E.g. change "from": "user" to "role": "user"
+        and "value" to "content"
+        and "gpt" to "assistant"
+
+    WizardLMTeam/WizardLM_evol_instruct_V2_196k
+    """
+    name_mapping = {
+        "gpt": "assistant",
+        "Assistant": "assistant",
+        "assistant": "assistant",
+        "user": "user",
+        "User": "user",
+        "human": "user",
+    }
+    messages = [{"role": name_mapping[conv["from"]], "content": conv["value"]} for conv in example["conversations"]]
+    example["messages"] = messages
+    return example
+
+
+def convert_rejection_samples_to_messages(example):
+    """
+    Convert a rejection sampling dataset to messages.
+    """
+    example["messages"] = example["chosen"]
+    return example
+
+def mix_datasets(
+    dataset_mixer: Union[dict, list],
+    splits: Optional[List[str]] = None,
+    configs: Optional[List[str]] = None,
+    columns_to_keep: Optional[List[str]] = None,
+    shuffle: bool = True,
+    save_data_dir: Optional[str] = None,
+    need_columns: Optional[List[str]] = None,
+    keep_ids: bool = False,
+    add_source_col: bool = False,
+) -> DatasetDict:
+    
+    """
+    dataset_mixer: Union[dict, list]
+        The dataset or datasets to be mixed. If a list, the datasets must be in the same order as the splits.
+        e.g. ["dataset_a", 1000, "dataset_b", 500] will mix 1000 samples from dataset_a and 500 samples from dataset_b.
+        ["dataset_a", 0.5, "dataset_b", 0.5] will mix 50% of dataset_a and 50% of dataset_b.
+        {"dataset_a": 1000, "dataset_b": 500} will mix 1000 samples from dataset_a and 500 samples from dataset_b.
+    splits (Optional[List[str]], *optional*, defaults to `None`):
+            Dataset splits to load and mix. Assumes the splits exist in
+            all datasets and have a `train_` or `test_` prefix.
+    configs (Optional[List[str]], *optional*, defaults to `None`):
+        List of dataset config names. If given must be the same length as 'dataset_mixer' keys.
+    columns_to_keep (Optional[List[str]], *optional*, defaults to `None`):
+        Column names to keep in the dataset. Useful in the datamixer to avoid schema conflicts,
+        and for cpt this should be (at least) the text column.
+    shuffle (`bool`, *optional*, defaults to `True`):
+        Whether to shuffle the training and testing/validation data.
+    save_data_dir (Optional[str], *optional*, defaults to `None`):
+        Optional directory to save training/test mixes on.
+    need_columns (Optional[List[str]], *optional*, defaults to `None`):
+        Column names that are required to be in the dataset.
+        Quick debugging when mixing heterogeneous datasets.
+    keep_ids (`bool`, *optional*, defaults to `False`):
+        Whether to keep ids for training that are added during mixing.
+        Used primarily in mix_data.py for saving, or the saved dataset has IDs already.
+    add_source_col (`bool`, *optional*, defaults to `False`):
+        Whether to add a column to the dataset that indicates the source of the data explicitly.
+    """
+    if isinstance(dataset_mixer, list):
+        assert len(dataset_mixer) % 2 == 0, f"Data mixer list length is not even: {dataset_mixer}"
+        mixer_dict = {}
+        i = 0
+        while i < len(dataset_mixer) - 1:
+            assert isinstance(dataset_mixer[i], str), f"Invalid type in data mixer: {dataset_mixer}"
+            if "." in dataset_mixer[i + 1]:
+                value = float(dataset_mixer[i + 1])
+            else:
+                value = int(dataset_mixer[i + 1])
+            mixer_dict[dataset_mixer[i]] = value
+            i += 2
+        dataset_mixer = mixer_dict
+    
+    splits = ["train", "test"] if splits is None else splits
+    configs = [None] * len(dataset_mixer) if not configs else configs
+    columns_to_keep = [] if columns_to_keep is None else columns_to_keep
+
+    if configs is not None and len(configs) != len(dataset_mixer):
+        raise ValueError("The number of given dataset config names must be the same as the given number of datasets.")
+
+    # print save location
+    if save_data_dir:
+        print(f"Saving mixed dataset to {save_data_dir}")
+
+    raw_datasets = DatasetDict()
+    raw_train_datasets = []
+    raw_val_datasets = []
+    frac_or_sample_list = []
+    for (ds, frac_or_samples), ds_config in zip(dataset_mixer.items(), configs):
+        frac_or_sample_list.append(frac_or_samples)
+        for split in splits:
+            # if dataset ends with .json or .jsonl, load from file
+            if ds.endswith(".json") or ds.endswith(".jsonl"):
+                dataset = load_dataset("json", data_files=ds, split=split)
+            else:
+                try:
+                    # Try first if dataset on a Hub repo
+                    dataset = load_dataset(ds, ds_config, split=split)
+                except DatasetGenerationError:
+                    # If not, check local dataset
+                    dataset = load_from_disk(os.path.join(ds, split))
+
+            # shuffle dataset if set
+            if shuffle:
+                dataset = dataset.shuffle(seed=42)
+
+            # assert that needed columns are present
+            if need_columns:
+                if not all(col in dataset.column_names for col in need_columns):
+                    raise ValueError(f"Needed column {need_columns} not found in dataset {dataset.column_names}.")
+
+            # handle per-case conversions
+            # if "instruction" and "output" columns are present and "messages" is not, convert to messages
+            if (
+                "instruction" in dataset.column_names
+                and "output" in dataset.column_names
+                and "messages" not in dataset.column_names
+            ):
+                dataset = dataset.map(convert_alpaca_gpt4_to_messages, num_proc=10)
+            elif (
+                "prompt" in dataset.column_names
+                and "completion" in dataset.column_names
+                and "messages" not in dataset.column_names
+            ):
+                dataset = dataset.map(convert_code_alpaca_to_messages, num_proc=10)
+            elif "conversations" in dataset.column_names and "messages" not in dataset.column_names:
+                dataset = dataset.map(conversations_to_messages, num_proc=10)
+            elif (
+                "question" in dataset.column_names
+                and "response" in dataset.column_names
+                and "messages" not in dataset.column_names
+            ):
+                dataset = dataset.map(convert_open_orca_to_messages, num_proc=10)
+            elif (
+                "query" in dataset.column_names
+                and "answer" in dataset.column_names
+                and "messages" not in dataset.column_names
+            ):
+                dataset = dataset.map(convert_codefeedback_single_turn_to_messages, num_proc=10)
+            elif (
+                "query" in dataset.column_names
+                and "response" in dataset.column_names
+                and "messages" not in dataset.column_names
+            ):
+                dataset = dataset.map(convert_metamath_qa_to_messages, num_proc=10)
+            elif (
+                "chosen" in dataset.column_names
+                and "rejected" in dataset.column_names
+                and "reference_completion" in dataset.column_names
+                and "messages" not in dataset.column_names
+            ):
+                dataset = dataset.map(convert_rejection_samples_to_messages, num_proc=10)
+
+            # if id not in dataset, create it as ds-{index}
+            if "id" not in dataset.column_names:
+                id_col = [f"{ds}_{i}" for i in range(len(dataset))]
+                dataset = dataset.add_column("id", id_col)
+
+            # Remove redundant columns to avoid schema conflicts on load
+            dataset = dataset.remove_columns(
+                [col for col in dataset.column_names if col not in (columns_to_keep + ["id"])]
+            )
+
+            # if add_source_col, add that column
+            if add_source_col:
+                source_col = [ds] * len(dataset)
+                dataset = dataset.add_column("source", source_col)
+
+            # for cols in columns_to_keep, if one is not present, add "None" to the column
+            for col in columns_to_keep:
+                if col not in dataset.column_names:
+                    dataset = dataset.add_column(col, [None] * len(dataset))
+
+            # add tag to the dataset corresponding to where it was sourced from, for
+            if "train" in split:
+                raw_train_datasets.append(dataset)
+            elif "test" in split:
+                raw_val_datasets.append(dataset)
+            else:
+                raise ValueError(f"Split type {split} not recognized as one of test or train.")
+
+    if len(raw_val_datasets) == 0 and len(raw_train_datasets) == 0:
+        raise ValueError("No datasets loaded.")
+    elif len(raw_train_datasets) == 0:
+        # target features are the features of the first dataset post load
+        target_features = raw_val_datasets[0].features
+    else:
+        # target features are the features of the first dataset post load
+        target_features = raw_train_datasets[0].features
+
+    if any(frac_or_samples < 0 for frac_or_samples in frac_or_sample_list):
+        raise ValueError("Dataset fractions / lengths cannot be negative.")
+
+    # if any > 1, use count
+    if any(frac_or_samples > 1 for frac_or_samples in frac_or_sample_list):
+        is_count = True
+        # assert that all are integers
+        if not all(isinstance(frac_or_samples, int) for frac_or_samples in frac_or_sample_list):
+            raise NotImplementedError("Cannot mix fractions and counts, yet.")
+    else:
+        is_count = False
+
+    if len(raw_train_datasets) > 0:
+        train_subsets = []
+        # Manage proportions
+        for dataset, frac_or_samples in zip(raw_train_datasets, frac_or_sample_list):
+            # cast features (TODO, add more feature regularization)
+            dataset = dataset.cast(target_features)
+            # TODO selection can be randomized.
+            if is_count:
+                train_subset = dataset.select(range(frac_or_samples))
+            else:
+                train_subset = dataset.select(range(int(frac_or_samples * len(dataset))))
+            train_subsets.append(train_subset)
+
+        raw_datasets["train"] = concatenate_datasets(train_subsets)
+
+    # No subsampling for test datasets to enable fair comparison across models
+    if len(raw_val_datasets) > 0:
+        for dataset in raw_val_datasets:
+            # cast features (TODO, add more feature regularization)
+            dataset = dataset.cast(target_features)
+
+        raw_datasets["test"] = concatenate_datasets(raw_val_datasets)
+
+    if len(raw_datasets) == 0:
+        raise ValueError(
+            f"Dataset {dataset_mixer} not recognized with splits {splits}."
+            "Check the dataset has been correctly formatted."
+        )
+
+    # optional save
+    if save_data_dir:
+        for split in raw_datasets:
+            raw_datasets[split].to_json(save_data_dir + f"mixed_ds_{split}.json")
+
+    if not keep_ids:
+        # remove id column
+        if len(raw_train_datasets) > 0:
+            if "id" in raw_datasets["train"].column_names:
+                raw_datasets["train"] = raw_datasets["train"].remove_columns("id")
+        if len(raw_val_datasets) > 0:
+            if "id" in raw_datasets["test"].column_names:
+                raw_datasets["test"] = raw_datasets["test"].remove_columns("id")
+
+    return raw_datasets
